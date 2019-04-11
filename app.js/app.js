cfe1d50a2c0fa91ac19e618db3d4b0dd8af7806c
rmsidsha <rmsidsha@gamil.com>
2019-04-11 19:24:20 +0900

    4.11

--- app.js/app.js
+++ app.js/app.js
@@ -7,37 +7,20 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var testRouter = require('./routes/test'); //사용자 지정
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
-// Createed user middle ware
-/* app.use(function(req, res, next){
-  console.log("I'm middle ware too");
-  next();  //next가 없으면 다음 것이 실행 안됨.
-});
- */
 app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-/*  ↓SAME WAY
-app.use(
-  logger('dev'),
-  espress.static(path.join(__dirname, 'public')),
-  express.json(),
-  express.urlencoded({ extended: false }),
-  cookieParse()
-); */
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/test', testRouter);//사용자 설정
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
